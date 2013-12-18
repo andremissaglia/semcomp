5261caa6006c0b7e31a75015079e211ba75ce3c7
André <andre@localhost.localdomain>
2013-12-18 13:40:01 -0200

    imagem mais leve, usando porcentagens.

--- php/index.php
+++ php/index.php
@@ -3,35 +3,12 @@
 <head>
 <title> Semcomp 17 </title>
 <meta charset="UTF-8">
-<style>
-*{
-	margin:0;
-	padding:0;
-}
-body{
-	background: #004f78;
-}
-#content{
-	width:800px;
-	margin: 0 auto;
-	text-align:center;
-}
-img{
-	width:800px;
-	height:600px;
-}
-a, a:hover, a:active, a:visited {
-	text-decoration:none;
-	color:white;
-	font-family: Calibri;
-	font-weight:bold;
-}
-</style>
+<link rel="stylesheet" href="layout.css">
 </head>
 <body>
 <div id="content">
 	<p><a href="http://www.facebook.com/semcomp">
-	<img src="site.png"/>
+	<img src="site.gif">
 	</a>
 	</p>
 
