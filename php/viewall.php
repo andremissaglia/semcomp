80a5e04948ef8cfee71c1b4f1fff8e25e316692e
andre <andre.missaglia@gmail.com>
2014-02-04 16:32:02 -0200

    Corrigido bug da submissão de emails.

--- php/viewall.php
+++ php/viewall.php
@@ -1,12 +1,14 @@
 <?php
 if(!isset($_GET['pass']) || $_GET['pass'] != "semcompFTW17"){
+	echo "Saindo, senha errada";
 	exit();
 }
 if(isset($_GET['ac']) && $_GET['ac'] == 'd'){
+	echo "Deletando lista"
 	unlink("email_list.txt");
 }
 if(isset($_GET['ac']) && $_GET['ac'] == 'l'){
+	echo "Exibindo lista";
 	print file_get_contents("email_list.txt");
 }
-system("ls");
 ?>
